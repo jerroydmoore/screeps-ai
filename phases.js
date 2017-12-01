3abe817937827c7ba312439f7f5266fa1d3aef52
Jerroyd Moore <jerroydmoore@gmail.com>
2017-12-01 15:53:14 -0800

    Update Phases so they don't spawn creeps that require roads in phase 1

--- phases.js
+++ phases.js
@@ -2,6 +2,7 @@ const StructExtensions = require('struct-extensions');
 let Phases = [
     {}, // Controller level starts at 0
     {
+        // < 1 Extension
         Level: 1,
         checkLevelPeriod: 100,
         SpawnScoutAfterSkippedPeriods: -1,
@@ -9,31 +10,32 @@ let Phases = [
         minimumEnergyToSpawn: 250,
         Harvester: {
             count: 2,
-            parts: [WORK,CARRY,MOVE,MOVE,CARRY,WORK,MOVE,WORK,CARRY]
+            parts: [WORK,CARRY,MOVE] // 250
         },
         Upgrader: {
             count: 4,
-            parts: [WORK,CARRY,MOVE,MOVE,CARRY,WORK,MOVE,WORK,CARRY]
+            parts: [WORK,CARRY,MOVE] // 250
         },
         Builder: {
             count: 4,
-            parts: [WORK,CARRY,MOVE,MOVE,CARRY,WORK,MOVE,WORK,CARRY]
+            parts: [WORK,CARRY,MOVE] // 250
         },
         Scout: {
-            count: 256,
+            count: 0,
             // scouts are expected to go on roads
-            parts: [TOUGH, MOVE, MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK ]
+            parts: [ ]
 
         },
         Pilgrim: {
-            count: 1,
-            parts: [MOVE, CLAIM, MOVE, WORK]
+            count: 0,
+            parts: []
         },
         RemoteHarvester: {
-            count: 6,
-            parts: [WORK,CARRY,MOVE,MOVE,CARRY,WORK,MOVE,WORK,CARRY]
+            count: 0,
+            parts: [ ]
         },
     }, {
+        // 1 <= Extensions < 5
         Level: 2,
         checkLevelPeriod: 500,
         SpawnScoutAfterSkippedPeriods: 10,
@@ -61,7 +63,39 @@ let Phases = [
             parts: [MOVE, MOVE, CLAIM]
         },
         RemoteHarvester: {
-            count: 6,
+            count: 0,
+            parts: [WORK,CARRY,MOVE,MOVE,CARRY,WORK,MOVE,WORK,CARRY]
+        },
+    }, {
+        // 5 <= Extensions
+        Level: 2,
+        checkLevelPeriod: 500,
+        SpawnScoutAfterSkippedPeriods: 10,
+        SpawnPeriod: 50,
+        minimumEnergyToSpawn: 550,
+        Harvester: {
+            count: 2,
+            parts: [WORK,CARRY,MOVE,MOVE,CARRY,WORK,MOVE,WORK,CARRY]
+        },
+        Upgrader: {
+            count: 4,
+            parts: [WORK,CARRY,MOVE,MOVE,CARRY,WORK,MOVE,WORK,CARRY]
+        },
+        Builder: {
+            count: 4,
+            parts: [WORK,CARRY,MOVE,MOVE,CARRY,WORK,MOVE,WORK,CARRY]
+        },
+        Scout: {
+            count: 256,
+            // scouts are expected to go on roads
+            parts: [TOUGH, MOVE, MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK, MOVE, ATTACK ]
+        },
+        Pilgrim: {
+            count: 1,
+            parts: [MOVE, MOVE, CLAIM]
+        },
+        RemoteHarvester: {
+            count: 0,
             parts: [WORK,CARRY,MOVE,MOVE,CARRY,WORK,MOVE,WORK,CARRY]
         },
     }
@@ -88,8 +122,11 @@ Phases.determineCurrentPhaseNumber = function (spawner) {
         // We don't need to check on every tick
         spawner.memory.phase = 1;
         let existingExt = StructExtensions.getMyStructs(spawner.room);
-        if (existingExt.length >= 5) {
+        if (existingExt.length > 1) {
             spawner.memory.phase = 2;
+        }
+        if (existingExt.length >= 5) {
+            spawner.memory.phase = 3;
         }
         if (phaseNo !== spawner.memory.phase) {
             console.log(`Updated ${spawner} phase to ${spawner.memory.phase}`);
