5ede47fd15673c734414175b6d1206f2d677782a
Jerroyd Moore <jerroydmoore@gmail.com>
2017-12-21 17:26:50 -0800

    game-init: initialize room entries

--- game-init.js
+++ game-init.js
@@ -10,5 +10,10 @@ module.exports = function(phaseNumber) {
     if (! Memory.decon) Memory.decon = {};
     if (! Memory.con) Memory.con = {};
     if (! Memory.rooms) Memory.rooms = {};
+    for (let room in Game.rooms) {
+        if (! Memory.rooms[room.name]) {
+            Memory.rooms[room.name] = {};
+        }
+    }
     if (! Memory.towers) Memory.towers = {};
 };
